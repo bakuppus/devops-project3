f12ba4d3b5bc42f17dd8bcb162cc402a096be7c4
root <root@ip-172-31-53-106.ec2.internal>
2019-03-29 17:24:01 +0000

    29032019

--- deploy-to-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export NODE_IP=$(kubectl describe svc dev-service | grep  addresses | cut -d '"' -f4)
-echo $NODE_IP
-curl -v -u admin:admin -T target/javaee7-simple-sample.war http://$NODE_IP:30000/manager/text/deploy?path=/dev&update=true
-echo "Done"
